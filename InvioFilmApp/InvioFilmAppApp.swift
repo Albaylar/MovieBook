71f6750bf0416f5b22a4f59fb31459631474440f
Albaylar <git@github.com:Albaylar/Countries.git>
2022-10-19 10:53:07 +0300

    Initial Commit

---
+++ InvioFilmApp/InvioFilmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvioFilmAppApp.swift
+//  InvioFilmApp
+//
+//  Created by Furkan Deniz Albaylar on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct InvioFilmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
